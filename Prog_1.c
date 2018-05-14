edfd98f7dac65292fb58469d0661c832951b55b3
Corey Stidston <coreystidston@Coreys-MacBook-Pro.local>
2018-05-14 12:42:47 +1000

    Added base source files and assignment files

---
+++ Prog_1.c
@@ -0,0 +1,14 @@
+/*
+ * Author: Corey Stidston
+ * Compilation method:
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char*argv[])
+{
+    printf("Hello world\n");
+    return(0);
+}
---
+++ Prog_2.c
@@ -0,0 +1,15 @@
+/*
+ * Author: Corey Stidston
+ * Compilation method:
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char*argv[])
+{
+    printf("Hello world");
+    return(0);
+}
+
