0582c31751d64d7ef629e54a8cf27fc038cf4b4f
Corey Stidston <coreystidston@Coreys-MacBook-Pro.local>
2018-05-18 16:34:10 +1000

    Working with threads

--- Prog_1.c
+++ Prog_1.c
@@ -7,9 +7,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
 
 #define NUM_PROCESSES 7
-#define FIFO_SIZE 2
+#define FIFO_PATH "/tmp/prog1fifo"
 
 typedef struct {
     int pId;
@@ -22,32 +27,71 @@ typedef struct {
     char const * const filename;
 } writing_args_t;
 
-//sem_t simulateCpuSchedulerSem, writeToFileSem; // Semaphores
-//pthread_t tidA, tidB; // Thread IDs
-//pthread_attr_t attr; // Thread attributes
+typedef struct {
+    char key[3];
+    float value;
+} key_value_pair_t;
+
+sem_t writeToFileSem; // Semaphores
+pthread_t tidA, tidB; // Thread IDs
+pthread_attr_t attr; // Thread attributes
 
 void initializeData(process_data_t *processData);
+void initializeSemaphore();
 void *simulateCpuScheduler(void *param);
 void *writeToFile(void *param);
 
 int main(int argc, char*argv[])
 {
-    unsigned int time = 0;
+    if(argc != 2)
+    {
+        printf("Please supply 1 argument: the output file name.\n"); exit(1);
+    }
+    
+    writing_args_t writingArgs = {argv[1]};
     
     process_data_t *processData;
-    processData = malloc(sizeof(process_data_t)*NUM_PROCESSES);// Allocate memory for processData
+    processData = malloc(sizeof(process_data_t)*NUM_PROCESSES);// Allocate Memory For Process Data
+    
     initializeData(processData);
+    initializeSemaphore();
+    
+    //if(mkfifo(FIFO_PATH, 0666) == -1)
+    //{
+    //    printf("Issue creating mkfifo.\n"); exit(1);
+    //}
+    
+    pthread_attr_init(&attr); // Get the default attributes
 
+    pthread_create(&tidA, &attr, simulateCpuScheduler, processData); // Creates Thread A
+    pthread_create(&tidB, &attr, writeToFile, &writingArgs); // Creates Thread B
+
+    // Wait For Thread B To Finish
+    if(pthread_join(tidB, NULL) != 0)
+    {
+        printf("Issue joining Thread B\n"); exit(1);
+    }
+
+    // Free Allocated Memory
+    free(processData);
+
+    return(0);
+}
+
+void *simulateCpuScheduler(void *param)
+{
+    process_data_t *processData = param;
     
-    /************START Thread 1: Simulate CPU Scheduling */
+    unsigned int time = 0; // Represents CPU ticks
     
-    // Initialize Average Waiting Time and Average Turn-around time
+    //int fd = open(FIFO_PATH, O_WRONLY); // Open mkfifo Pipe With Write Only Permission
+    
+    // Initialize Variables To Calculate Average Waiting Time and Average Turn-around Time 
     double waitingTime = 0;
     double turnaroundTime = 0;
-    double averageWaitingTime;
-    double averageTurnaroundTime;
+    double averageWaitingTime = 0;
+    double averageTurnaroundTime = 0;
     
-    // Have some kind of loop here
     int indexOfSmallestCpuBurstTime;
     int numProcessesComplete = 0;
     
@@ -56,7 +100,8 @@ int main(int argc, char*argv[])
         // Find The Process with the Smallest Burst Time
         indexOfSmallestCpuBurstTime = -1;
         bool first = true;
-        for (int i = 0; i < NUM_PROCESSES; i++)
+        int i;
+        for (i = 0; i < NUM_PROCESSES; i++)
         {
             if(time >= processData[i].arriveTime && processData[i].remainingBurstTime > 0)
             {
@@ -68,25 +113,22 @@ int main(int argc, char*argv[])
             }
         }
         
-        //Simulate CPU Clock
+        // Simulate CPU Clock
         ++time;
         
         if(indexOfSmallestCpuBurstTime != -1) // Check For A Valid Index
         {
-            //printf("executing process: %i\n", indexOfSmallestCpuBurstTime + 1);
             processData[indexOfSmallestCpuBurstTime].remainingBurstTime -= 1; // Simulate CPU Burst
             
             if(processData[indexOfSmallestCpuBurstTime].remainingBurstTime == 0) // Check For Completed Process
             {
-                //printf("finished process: %i at time: %i\n", indexOfSmallestCpuBurstTime+1, time);
-                
                 // Summate Wait Time (wait time = end time - arrival time - burst time)
                 waitingTime += time - processData[indexOfSmallestCpuBurstTime].arriveTime - processData[indexOfSmallestCpuBurstTime].burstTime;
                 
                 // Summate Turn-around Time (turn-around time = end time - arrive time)
                 turnaroundTime += time - processData[indexOfSmallestCpuBurstTime].arriveTime;
                 
-                // Increment The Number of Processes Complete
+                // Increment The Number of Processes CompletesimulateCpuSchedulerSem
                 ++numProcessesComplete;
             }
         }
@@ -98,72 +140,43 @@ int main(int argc, char*argv[])
     printf("Average waiting time = %f \n", averageWaitingTime);
     printf("Average turn-around time = %f \n", averageTurnaroundTime);
     
-    
-    // Calculate average waiting time and average turn-around time
-    
     // Push to FIFO
-    
-    // Signal Thread 2
-    
-    /************ END Thread 1 */
-    
-    //writing_args_t writingArgs = {argv[1]};
-    
-    
-//    pthread_attr_init(&attr); // Get the default attributes
-//
-//    pthread_create(&tidA, &attr, simulateCPUScheduler, processData); // Creates thread A
-//    pthread_create(&tidB, &attr, writeToFile, &writingArgs); // Creates thread B
-//
-//    if(pthread_join(tidA, NULL) != 0)
-//        printf("Issue joining Thread A\n"); exit(1);
-//    if(pthread_join(tidB, NULL) != 0)
-//        printf("Issue joining Thread B\n"); exit(1);
-    
-    
-    // Free Allocated Memory
-    free(processData);
+    //key_value_pair_t averageWaitingTime_t = {"AWT", averageWaitingTime};
+    //key_value_pair_t *test;
 
-    return(0);
-}
-
-void *simulateCpuScheduler(void *param)
-{
+    //write(fd, &averageWaitingTime, sizeof(double));
+    //close(fd);
+    
+    sem_post(&writeToFileSem);
+    
     return 0;
 }
 
 void *writeToFile(void *param)
 {
-//    writing_args_t *parameters = param;
-//    FILE* writeFile = fopen(parameters->filename, "w");
-//    if(!writeFile)
-//    {
-//        perror("Invalid File");
-//        exit(1);
-//    }
-//
-//    char line[BUFFER_SIZE];
-//    int eoh_flag = 0; // End of header flag
-//
-//    while(!sem_wait(&writeToFileSem))
-//    {
-//        if(eoh_flag)
-//        {
-//            /* Place the contents of the shared buffer into file */
-//            fputs(parameters->shared_buffer->buffer, writeFile);
-//        }
-//        else if(strstr(parameters->shared_buffer->buffer, END_OF_HEADER)) // Check for the end of header
-//        {
-//            eoh_flag = 1;
-//        }
-//
-//        sem_post(&readSem);
-//    }
-//
-//    fclose(writeFile); // Close FILE*
+    writing_args_t *parameters = param;
+    FILE* writeFile = fopen(parameters->filename, "w");
+    if(!writeFile)
+    {
+       perror("Invalid File");
+       exit(1);
+    }
     
-//    pthread_cancel(tidA);
-//    pthread_cancel(tidB);
+    //int fd = open(FIFO_PATH, O_RDONLY); // Open mkfifo Pipe With Read Only Permission
+
+    sem_wait(&writeToFileSem);
+    
+    //double *averageWaitingTime;
+    //read(fd, averageWaitingTime, sizeof(double));
+    //printf("hi -> %f <-\n", *averageWaitingTime);
+
+    fputs("hello12345", writeFile);
+
+    fclose(writeFile); // Close FILE*
+    //close(fd);
+    
+    pthread_cancel(tidA);
+    pthread_cancel(tidB);
 
     return 0;
 }
@@ -177,4 +190,15 @@ void initializeData(process_data_t *processData)
     processData[4].pId = 5; processData[4].arriveTime = 16; processData[4].burstTime = 4; processData[4].remainingBurstTime = 4;
     processData[5].pId = 6; processData[5].arriveTime = 21; processData[5].burstTime = 6; processData[5].remainingBurstTime = 6;
     processData[6].pId = 7; processData[6].arriveTime = 26; processData[6].burstTime = 2; processData[6].remainingBurstTime = 2;
+}
+
+void initializeSemaphore()
+{   
+    if(sem_init(&writeToFileSem, 0, 0))
+    {
+        perror("Error Initializing Semaphore.\n");
+        exit(1);
+    }
+    
+    pthread_attr_init(&attr);
 }
